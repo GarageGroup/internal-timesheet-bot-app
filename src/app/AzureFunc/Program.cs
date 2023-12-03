6f6fe9f80928efd3503dbf7693318767182ff31c
pmosk <pm@ggroupp.ru>
2023-12-03 23:54:07 +0400

    Add appsettings.json

--- src/app/AzureFunc/Program.cs
+++ src/app/AzureFunc/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace GarageGroup.Internal.Timesheet;
@@ -8,6 +10,9 @@ static class Program
     static Task Main()
         =>
         Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration(
+            static (context, builder) => builder.AddJsonFile(
+                Path.Combine(context.HostingEnvironment.ContentRootPath, "appsettings.json"), optional: false))
         .ConfigureFunctionsWorkerStandard(
             useHostConfiguration: false,
             configure: Application.Configure)
